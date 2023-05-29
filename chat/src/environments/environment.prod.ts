3e5fca6eee434c7be47b798bb67408857508128e
Brayan-C-Avila <brayant115@outlook.es>
2023-05-29 12:29:33 -0600

    Primer version - PLACES

---
+++ chat/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true
+};
+
+ export const firebaseConfig = {
+  apiKey: "AIzaSyB1LKcrzePC4KMtTTrcSXmg0KMuSCXacxs",
+  authDomain: "chatonline-586ae.firebaseapp.com",
+  databaseURL: "https://chatonline-586ae-default-rtdb.firebaseio.com",
+  projectId: "chatonline-586ae",
+  storageBucket: "chatonline-586ae.appspot.com",
+  messagingSenderId: "346682119747",
+  appId: "1:346682119747:web:3ffe396b43a7bec1d526f6"
+};
